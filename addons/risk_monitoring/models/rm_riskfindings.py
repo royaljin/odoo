7a2a8491717fceeffd64006ee94e0e9b25897d98
royaljin <65331166+royaljin@users.noreply.github.com>
2020-10-14 02:15:38 +0530

    Update rm_riskfindings.py

--- addons/risk_monitoring/models/rm_riskfindings.py
+++ addons/risk_monitoring/models/rm_riskfindings.py
@@ -1,24 +1,18 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-import base64
-from random import choice
-from string import digits
-from werkzeug.urls import url_encode
 
-from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError, AccessError
-from odoo.modules.module import get_module_resource
+from odoo import fields, models
 
 
 class RiskFindings(models.Model):
-    _name = "rm.RiskFindings"
+    _name = "rm.riskfindings"
     _description = "Findings of Asset Risks"
     _order = 'name'
 
     risk_ID = fields.Char()
     name = fields.Char(string="Risk Description", store=True, readonly=False, tracking=True)
-    FindingDate = fields.date("Date of Findings")
+    FindingDate = fields.Date("Date of Findings")
     note = fields.Text('Note')
 
-    Asset_name = fields.Many2one('rm.InvestedAssets', 'Asset_ID','Asset Name')
+    #Asset_name = fields.Many2one('rm.investedassets', 'Asset_ID','Asset Name')
